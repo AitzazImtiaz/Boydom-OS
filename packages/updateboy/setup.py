b5316187d008867faf55cd38338bc2ec484807ad
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-18 22:34:32 +0500

    Create setup.py

---
+++ packages/updateboy/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "badboy",
+    version = "0.1.0",
+    description = "A evil version for evil people",
+    author = "AitzazImtiaz",
+    url = "https://github.com/AitzazImtiaz/Bad-Boy",
+    packages = ["updateboy"],
+    entry_points = {
+        'console_scripts': [
+            'updateboy = updateboy.__main__:main'
+        ]
+    },
+)
