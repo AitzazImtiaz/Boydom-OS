56862cc54f940d9d1da9285ae521cd1cb4968b19
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-18 20:26:14 +0500

    Create __main__.py

---
+++ packages/updateboy/__main__.py
@@ -0,0 +1,12 @@
+import os
+import time
+def main():
+  os.system("espeak 'There are no feelings, only calculations'")
+  time.sleep(2)
+  os.system("espeak 'Running update, Boydom update patch running!'")
+  print("Updating and reinstalling Boydom.")
+  time.sleep(1)
+  os.system("git clone https://github.com/AitzazImtiaz/Boydom-OS")
+  os.system("cd Boydom-OS")
+  os.system("chmod +x install.sh")
+  os.system("./install.sh")
