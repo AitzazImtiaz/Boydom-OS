5a2eda032a8f49c63e43d348a1b962a3eb9dd7b3
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-18 22:31:19 +0500

    Update and rename setup.py to installation.sh

---
+++ packages/installation.sh
@@ -0,0 +1,13 @@
+cd updateboy
+make install
+cd
+cd Boydom-OS/packages/helpboy
+make install
+cd
+cd Boydom-OS/packages/colorboy
+make install
+cd
+cd Boydom-OS/packages/binboy
+make install
+cd
+cd Boydom-OS
