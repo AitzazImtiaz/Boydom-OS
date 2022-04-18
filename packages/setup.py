ffd8692623dbdb5aec08c4fcb58dd35165ec6010
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-18 20:28:28 +0500

    Update setup.py

--- packages/setup.py
+++ packages/setup.py
@@ -6,12 +6,13 @@ setup(
     description = "A version if boys rule 😎",
     author = "AitzazImtiaz",
     url = "https://github.com/AitzazImtiaz/Boydom-OS",
-    packages = ["binboy", "helpboy", "updateboy"],
+    packages = ["binboy", "helpboy", "updateboy", "colorboy"],
     entry_points = {
         'console_scripts': [
             'binboy = binboy.__main__:main'
             'helpboy = helpboy._main_:main'
             'updateboy = updateboy._main_:main'
+            'colorboy = colorboy._main_:main'
         ]
     },
 )
