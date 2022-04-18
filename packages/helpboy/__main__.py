90502d0133254d1fd99a43439cd7dd5aeb653865
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-18 20:08:12 +0500

    Create __main__.py

---
+++ packages/helpboy/__main__.py
@@ -0,0 +1,7 @@
+import os
+from colorama import Fore
+def main():
+  os.system("espeak 'Helpboy loaded, Here are Built In commands'")
+  print(Fore.GREEN+"binboy", end=" ")
+  print(Fore.WHITE+"Print Binary colors")
+  
