8f2c5caac55e579d8028e534fd59c3dd27ec36b3
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-19 08:41:46 +0500

    Update __main__.py

--- packages/helpboy/helpboy/__main__.py
+++ packages/helpboy/helpboy/__main__.py
@@ -4,4 +4,8 @@ def main():
   os.system("espeak 'Helpboy loaded, Here are Built In commands'")
   print(Fore.GREEN+"binboy", end=" ")
   print(Fore.WHITE+"Print Binary colors")
+  print(Fore.GREEN+"updateboy", end=" ")
+  print(Fore.WHITE+"Update Boydom to a new version")
+  print(Fore.GREEN+"colorboy", end=" ")
+  print (Fore.WHITE+"Print all console colors in a pattern")
   
