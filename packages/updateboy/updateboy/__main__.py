0af1a1c960bf20cba358b4e089f4d32d9dc87b32
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-19 08:50:29 +0500

    Update __main__.py

--- packages/updateboy/updateboy/__main__.py
+++ packages/updateboy/updateboy/__main__.py
@@ -1,13 +1,8 @@
 import os
 import time
-def main():
-  os.system("espeak 'There are no feelings, only calculations'")
-  time.sleep(2)
-  os.system("espeak 'Running update, Boydom update patch running!'")
-  print("Updating and reinstalling Boydom.")
-  time.sleep(1)
-  os.system("rm Boydom-OS -r")
-  os.system("git clone https://github.com/AitzazImtiaz/Boydom-OS")
-  os.system("cd Boydom-OS")
-  os.system("chmod +x install.sh")
-  os.system("./install.sh")
+os.system("espeak 'There are no feelings, only calculations'")
+time.sleep(2)
+os.system("espeak 'Running update, Boydom update patch running!'")
+print("Updating and reinstalling Boydom.")
+time.sleep(1)
+os.sytem("bash updateboy.sh")
