7523e1936c586cf8f353e6060b328d49a5ef3c87
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-26 20:48:27 +0500

    Create hasher.py

---
+++ packages/badboy/badboy/scripts/hasher.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# Written By: Sahar Hathiramani
+# Date: 01/13/2021
+import os
+import hashlib
+os.system("clear")
+print("🄱🄰🄳 🄱🄾🅈 🄱🅄🅃 🄰 🅂🄰🄳 🄱🄾🅈")
+
+hashValue = input('Enter String to Hash: ')
+
+hashmd5 = hashlib.md5()
+hashmd5.update(hashValue.encode())
+print('MD5 Hash: ' + hashmd5.hexdigest())
+
+hashsha1 = hashlib.sha1();
+hashsha1.update(hashValue.encode())
+print('SHA1 Hash: ' + hashsha1.hexdigest())
+
+hashsha224 = hashlib.sha224()
+hashsha224.update(hashValue.encode())
+print('SHA224 Hash: ' + hashsha224.hexdigest())
+
+hashsha256 = hashlib.sha256()
+hashsha256.update(hashValue.encode())
+print('SHA256 Hash: ' + hashsha256.hexdigest())
+
+hashsha512 = hashlib.sha512()
+hashsha512.update(hashValue.encode())
+print('SHA512 Hash: ' + hashsha512.hexdigest())
+n = str(input("Enter y to return back:"))
+if n=="y":
+  os.system("clear")
